e4bd71b265eeea34687db95f0cfce0d1e53699a7
Rich Leonard <rich@homeimprovement-li.com>
2017-02-03 14:12:31 -0500

    work in progress

--- apps/app.js
+++ apps/app.js
@@ -5,12 +5,28 @@ $(function(){
     });
   });
 
-  function showResults(results){
-      var html = "";
-$.each(results, function(index, value){
+$(function(){
+    $('#search-term').submit(function(event){
+        event.preventDefault();
+        var searchTerm = $('#jquery').val();
+        getRequest(searchTerm);
+      });
+    });
+
+function getRequest(searchTerm){
+  $.getJson('http://www.omdbapi.com/?s=' + searchTerm + '&r=json', function(data){
+            showResults(data.Search);  
+  });
+}
+
+function showResults(results){
+    var html = "";
+    $.each(results, function(index, value){
     html+= '<p>'+ value.Title + '<p>';   
-     console.log(value.Title);
- });
- $('#search-results').html(html);
+    console.log(value.Title);
+    
+    });
+
+    $('#search-results').html(html);
 }
 
