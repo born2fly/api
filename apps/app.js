abd34b080aa55c9a202bc58bfe35696918474eb3
Rich Leonard <rich@homeimprovement-li.com>
2017-02-04 13:57:40 -0500

    Tidy'd up code & did JS Refactor

--- apps/app.js
+++ apps/app.js
@@ -1,32 +1,37 @@
-$(function(){
+$(function () {
 
-    $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+    $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function (data) {
         showResults(data.Search);
     });
-  });
+});
 
-$(function(){
-    $('#search-term').submit(function(event){
+$(function () {
+    $('#search-term').submit(function (event) {
         event.preventDefault();
         var searchTerm = $('#jquery').val();
         getRequest(searchTerm);
-      });
     });
+});
 
-function getRequest(searchTerm){
-  $.getJson('http://www.omdbapi.com/?s=' + searchTerm + '&r=json', function(data){
-            showResults(data.Search);  
-  });
+function getRequest(searchTerm) {
+    var params = {
+        s: searcTerm,
+        r: 'json"'
+    };
+    url = 'http://www.omdbapi.com';
+
+    $.getJson(url, params, function (data) {
+        showResults(data.Search);
+    });
 }
 
-function showResults(results){
+function showResults(results) {
     var html = "";
-    $.each(results, function(index, value){
-    html+= '<p>'+ value.Title + '<p>';   
-    console.log(value.Title);
-    
+    $.each(results, function (index, value) {
+        html += '<p>' + value.Title + '<p>';
+        console.log(value.Title);
+
     });
 
     $('#search-results').html(html);
 }
-
